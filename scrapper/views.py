e4743f676a67a83479a4ea90f9c4d1c6296713d8
Paulo Antonio <anj.heo@gmail.com>
2018-09-17 18:35:28 -0300

    Changed scrap() to include number of results in response.

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,8 +1,10 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
 
 @api_view(['POST'])
 def scrap(request):
@@ -10,7 +12,16 @@ def scrap(request):
         page = requests.get(request.data['url'])
 
         soup = BeautifulSoup(page.text, features="html.parser")
-        data = []
+        
+        results = re.findall('\d+', soup.find_all("h2")[1].text)
+        
+        if len(results) == 0:
+            return Response({'data': [], 'numberOfResults': 0}, page.status_code)
+        
+        response = {
+            'numberOfResults': results[0] if len(results) == 1 else results[1],
+            'data': []
+        }
 
         containers = soup.find_all("div", class_="sentence-and-translations")
 
@@ -26,15 +37,14 @@ def scrap(request):
                     "direct": 'direct' in translation.find_parent("div").get("class")
                 })
                 
-            data.append({
+            response['data'].append({
                 "text": container.find(class_="text").text.strip(),
                 "language": container.find(class_="lang").find("img").get('alt'),
                 "id": container.find("md-button").get("href")[20::],
                 "translations": translations
             })
-
-            # TODO - add the links for the next and previous pages.
-
-        return Response(data, status=page.status_code)
+            
+        return Response(response, page.status_code)
     except Exception as e:
         return Response(status=500)
+
