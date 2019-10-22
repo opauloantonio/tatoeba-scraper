d1f17b320a76d916662b85beef57b5737ff32025
Paulo Antonio <anj.heo@gmail.com>
2019-10-22 20:59:52 -0300

    Fixed ids

--- scraper/views.py
+++ scraper/views.py
@@ -38,17 +38,18 @@ def search(request):
                     "id": translation.find_all(class_="md-icon-button")[-1].get("href")[20::],
                     "direct": 'direct' in translation.find_parent("div").get("class")
                 })
-                
+
+            sentence = container.find_all(class_='sentence')[0]
+
             response['data'].append({
-                "text": container.find(class_="text").text.strip(),
-                "language": container.find(class_="lang").find("img").get('alt'),
-                "id": container.find_all(class_="md-icon-button")[-1].get("href")[20::],
-                "translations": translations
+                'text': sentence.find('div', class_='text').text.strip(),
+                'language': sentence.find('img').get('alt'),
+                'id': sentence.find_all(class_="md-icon-button")[-1].get("href")[20::],
+                'translations': translations
             })
             
         return Response(response, page.status_code)
     except Exception as e:
-        print(request.data['url'])
         print(e)
         return Response(status=500)
 
@@ -58,16 +59,25 @@ def sentence_details(request):
     try:
         page = requests.get(request.data['url'])
         soup = BeautifulSoup(page.text, features='html.parser')
+        
+        translations = []
 
-        data = []
-
-        for x in soup.find_all(class_='sentence'):
-           data.append({
-               'text': x.find('div', class_='text').text,
-               'language': x.find('img').get('alt'),
-               'id': x.get('data-sentence-id'),
-               'direct': 'directTranslation' in x.get('class')
+        for t in soup.find_all(class_='translation'):
+            translations.append({
+                'text': t.find('div', class_='text').text.strip(),
+                'language': t.find('img').get('alt'),
+                'id': t.find_all(class_="md-icon-button")[-1].get("href")[20::],
+                "direct": 'direct' in t.find_parent("div").get("class")
             })
+
+        sentence = soup.find_all(class_='sentence')[0]
+
+        data = {
+            'text': sentence.find('div', class_='text').text.strip(),
+            'language': sentence.find('img').get('alt'),
+            'id': sentence.find_all(class_="md-icon-button")[-1].get("href")[20::],
+            'translations': translations
+        }
         
         return Response(data, page.status_code)
     except Exception as e:
