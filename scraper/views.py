a98a51bb0733bd50ae425cc08db19e4178507db5
Paulo Antonio <anj.heo@gmail.com>
2020-02-16 16:08:43 -0300

    Updated methods to work with current versions of Tatoeba

--- scraper/views.py
+++ scraper/views.py
@@ -6,79 +6,55 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
+null = None
+false = False
+true = True
+
+
+def get_sentence(source):
+    # The next two lines are absolutely the most horrible lines I've written in five years!
+
+    direct = [{**t, "direct": True} for t in [{k: sentence[k] for k in ('text', 'id', 'lang')} for sentence in source[1]]]
+    indirect = [{**t, "direct": False} for t in [{k: sentence[k] for k in ('text', 'id', 'lang')} for sentence in source[2]]]
+    
+    return {
+        "translations": direct + indirect,
+        "text": source[0]['text'],
+        "lang": source[0]['lang'],
+        "id": source[0]['id'],
+    }
+
+
 @api_view(['POST'])
 def search(request):
-    try:
-        page = requests.get(request.data['url'])
+    page = requests.get(request.data['url'])
 
-        soup = BeautifulSoup(page.text, features="html.parser")
+    soup = BeautifulSoup(page.text, features="html.parser")
+    results = soup.find_all("h2")[0].text
 
-        results = soup.find_all("h2")[0].text.split()[-2]
+    response = {
+        'numberOfResults': results,
+        'data': []
+    }
 
-        if len(results) == 0:
-            return Response({'data': [], 'numberOfResults': 0}, page.status_code)
+    containers = soup.find_all("div", class_="sentence-and-translations")
 
-        response = {
-            'numberOfResults': results,
-            'data': []
-        }
-
-        containers = soup.find_all("div", class_="sentence-and-translations")
-
-        for container in containers:
-            translations = []
-            translationsContainers = container.findAll(class_="translation")
-
-            for translation in translationsContainers:
-                translations.append({
-                    "text": translation.find(class_="text").text.strip(),
-                    "language": translation.find(class_="lang").find("img").get('alt'),
-                    "id": translation.find_all(class_="md-icon-button")[-1].get("href")[20::],
-                    "direct": 'direct' in translation.find_parent("div").get("class")
-                })
-
-            sentence = container.find_all(class_='sentence')[0]
-
-            response['data'].append({
-                'text': sentence.find('div', class_='text').text.strip(),
-                'language': sentence.find('img').get('alt'),
-                'id': sentence.find_all(class_="md-icon-button")[-1].get("href")[20::],
-                'translations': translations
-            })
-            
-        return Response(response, page.status_code)
-    except Exception as e:
-        #print(e)
-        logging.exception(e)
-        return Response(status=500)
+    for c in containers:
+        response['data'].append(get_sentence(eval(c.get("ng-init")[12:-1])))
+        
+    return Response(response, page.status_code)
 
 
 @api_view(['POST'])
 def sentence_details(request):
     try:
         page = requests.get(request.data['url'])
+
         soup = BeautifulSoup(page.text, features='html.parser')
+
+        container = soup.find("div", class_="sentence-and-translations")
         
-        translations = []
-
-        for t in soup.find_all(class_='translation'):
-            translations.append({
-                'text': t.find('div', class_='text').text.strip(),
-                'language': t.find('img').get('alt'),
-                'id': t.find_all(class_="md-icon-button")[-1].get("href")[20::],
-                "direct": 'direct' in t.find_parent("div").get("class")
-            })
-
-        sentence = soup.find_all(class_='sentence')[0]
-
-        data = {
-            'text': sentence.find('div', class_='text').text.strip(),
-            'language': sentence.find('img').get('alt'),
-            'id': sentence.find_all(class_="md-icon-button")[-1].get("href")[20::],
-            'translations': translations
-        }
-        
-        return Response(data, page.status_code)
+        return Response(get_sentence(eval(container.get("ng-init")[12:-1])), page.status_code)
     except Exception as e:
         print(e)
         return Response(status=500)
